888ffa3fb8eb2f1c3c762851c199d18c42590623
MaelitoP <makibeardy@gmail.com>
2025-06-20 19:32:41 +0200

    feat(lsp) add support for for typescript using biome

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -67,10 +67,24 @@ vim.lsp.config.nil_ls = {
   root_markers = { 'flake.nix', '.git' },
 }
 
+vim.lsp.config.tsserver = {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+}
+
+vim.lsp.config.biome = {
+  cmd = { 'biome', 'lsp-proxy' },
+  filetypes = { 'javascript', 'javascriptreact', 'json', 'jsonc', 'typescript', 'typescriptreact', 'astro', 'svelte', 'vue', 'css' },
+  root_markers = { 'biome.json', 'biome.jsonc' },
+  single_file_support = false,
+}
+
 vim.lsp.enable({
   'lua_ls',         -- Lua
   'rust_analyzer',  -- Rust
   'tsserver',       -- TypeScript & JavaScript
+  'biome',          -- Biome (JS/TS/JSON/CSS)
   'intelephense',   -- PHP
   'pyright',        -- Python
   'ocamllsp',       -- OCaml
